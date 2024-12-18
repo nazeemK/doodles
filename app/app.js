8d0ed83ed2a5704b16bcfc28ff9022fbadec766b
nk92 <khodabux.n@gmail.com>
2024-12-18 13:54:00 +0400

    Changed tasks list container from div to <li>

--- app/app.js
+++ app/app.js
@@ -1,23 +1,27 @@
 'use strict';
 
 // Declare app level module which depends on views, and core components
-const app = angular.module('todoApp', []);
+angular.module('todoApp', [])
+    .controller('TodoController', function($scope) {
+      $scope.tasks = [];
 
-app.controller('TodoController', function ($scope) {
-  $scope.tasks = [];
+      // Add a new task
+      $scope.addTask = function() {
+        if ($scope.newTask) {
+          const task = {
+            text: $scope.newTask,
+            dateAdded: new Date().toLocaleString(),
+            number: $scope.tasks.length + 1  // Set the task number
+          };
+          $scope.tasks.push(task);
+          $scope.newTask = '';  // Clear the input field
+        }
+      };
 
-  $scope.addTask = function () {
-    if ($scope.newTask) {
-      const currentDate = new Date(); // Get current date
-      $scope.tasks.push({
-        text: $scope.newTask,
-        dateAdded: currentDate.toLocaleString() // Format date
-      });
-      $scope.newTask = ''; // Clear input
-    }
-  };
-
-  $scope.removeTask = function (index) {
-    $scope.tasks.splice(index, 1);
-  };
-});
+      // Remove a task
+      $scope.removeTask = function(index) {
+        $scope.tasks.splice(index, 1);
+        // Update task numbers after removal
+        $scope.tasks.forEach((task, i) => task.number = i + 1);
+      };
+    });
